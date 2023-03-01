8cc1a94434cadcd62513a7b6033dd93321e2c0af
Andrea-Pomati <pomati.andrea@gmail.com>
2023-03-01 14:35:30 +0100

    feat: javascript carousel

--- js/script.js
+++ js/script.js
@@ -46,19 +46,148 @@
 // 3. Al momento giusto (ihihhi starà a voi capire quale)
 //  rispondete a questa domanda: "Quanti cicli servono?"
 
-// preparo un array di immagini
-
-const images = ["img/01webp","img/02webp","img/03webp","img/04webp","img/05webp","img/06webp"]
-
-// associo le variabili const agli elementi html 
-
-const upArrowEl = document.getElementById("up-arrow")
-const carouselActiveImg = document.getElementById("_img-carousel")
-const downArrowEl = document.getElementById("down-arrow")
-
-// memorizzo una variabile di indice che parte da 0
-
-let index = 0
 
 
-
+// - preparare un array di path di immagini
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+  ];
+  
+  // - preparare gli elementi in js corrispondenti agli elementi html
+  const leftArrowElement = document.getElementById("left-arrow");
+  const rightArrowElement = document.getElementById("right-arrow");
+  const activeImgElement = document.getElementById("carousel-active-img");
+  const dotsContainerElement = document.getElementById("dots-container");
+  const carouselThumbnailsElement = document.getElementById("carousel-thumbnails");
+  
+  // genero i pallini  
+  for(let i = 0; i < images.length; i++) {
+    // genero un elemento html di tag <div>
+    let newDot = document.createElement('div');
+    // ci aggiungo la classe "dot", necessaria per la stilizzazione
+    newDot.classList.add("dot");
+    // lo imposto come figlio di dotsContainerElement
+    dotsContainerElement.append(newDot);
+  }
+  
+  
+  // - memorizzare una variabile di indice
+  let index = 0;
+  
+  // inizio inserendo dentro il parametro src il valore della prima immagine dall'array
+  activeImgElement.src = images[index];
+  
+  
+  // prendo tutti i dots
+  const dotElements = document.querySelectorAll('.dot');
+  // aggiungo al primo la classe "active"
+  dotElements[index].classList.add("active");
+  
+  
+  
+  // - creare n miniature di immagine in base alla dimensione dell'array
+  for (let i = 0; i < images.length; i++) {
+    let newThumbnail = document.createElement('img');
+  
+    // appendo le immagini a #carousel-thumbnails
+    carouselThumbnailsElement.append(newThumbnail);
+  
+    // cambio il parametro src in modo che contenga il path dell'immagine relativa
+    newThumbnail.src = images[i];
+  
+    // aggiungo la classe thumbnail
+    newThumbnail.classList.add("thumbnail");
+  
+    // aggiungo l'attributo alt
+    newThumbnail.alt = "thumbnail picture";
+  
+    // attribuire lo stile in maniera dinamica
+    // - stilizzo l'altezza di ciascuna con la formula calc(100% / numero immagini)
+    newThumbnail.style.height = `calc(100% / ${images.length})`;
+  }
+  
+  // prendo le immagini create dal documento usando il selettore con la classe che gli ho appena aggiunto
+  const thumbnailElements = document.querySelectorAll("#carousel-thumbnails .thumbnail");
+  // aggiungo la classe active alla prima immagine
+  thumbnailElements[index].classList.add("active")
+  
+  
+  
+  
+  
+  
+  rightArrowElement.addEventListener("click", function () {
+    
+    // rimuoviamo la classe active al pallino corrispondente all'indice
+    dotElements[index].classList.remove("active");
+  
+    // - rimuovere la classe active dall'immagine con l'indice attuale (non ancora aumentato)
+    thumbnailElements[index].classList.remove("active");
+  
+    // ? SE l'indice è lunghezza dell'array - 1
+    if(index == images.length - 1) {
+  
+      // ° cambia l'indice con lo 0
+      index = 0;
+  
+    } else {
+  
+      // ° aumentare il valore dell'indice di un'unità
+      index++;
+  
+    }
+  
+    // ° aggiungi la classe active al cerchio relativo alla posizione dell'indice
+    dotElements[index].classList.add("active");
+  
+    //   ° mostrare l'immagine alla posizione dell'array relativa al valore dell'indice
+    activeImgElement.src = images[index];
+  
+    console.log(index)
+   
+  
+    // ° aggiungere la classe active all'immagine miniatura relativa alla posizione dell'indice
+    thumbnailElements[index].classList.add("active");
+  });
+  
+  
+  leftArrowElement.addEventListener("click", function () {
+    // rimuoviamo la classe active al pallino corrispondente all'indice
+    dotElements[index].classList.remove("active");
+  
+    // - rimuovere la classe active dall'immagine con l'indice attuale (non ancora aumentato)
+    thumbnailElements[index].classList.remove("active");
+  
+    // ? SE l'indice è 0
+    if(index == 0) {
+  
+      // ° cambia l'indice con la lunghezza dell'array - 1
+      index = images.length - 1;
+  
+    } else {
+  
+      //   ° diminuire il valore dell'indice di un'unità
+      index--;
+  
+    }
+  
+    
+    dotElements[index].classList.add("active");
+  
+    // ° aggiungere la classe active all'immagine miniatura relativa alla posizione dell'indice
+    thumbnailElements[index].classList.add("active");
+  
+  
+    //   ° mostrare l'immagine alla posizione dell'array relativa al valore dell'indice
+    activeImgElement.src = images[index];
+  
+    console.log(index)
+  
+    
+  
+  
+  });
